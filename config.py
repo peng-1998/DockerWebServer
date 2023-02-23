c2b554a5e7a970ff1f7633bb5f4b6d87d6a8e878
peng-1998 <1471881859@qq.com>
2023-02-23 16:36:43 +0800

    1

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ docker_images_database_save_path = 'data/database/docker_images.json'
 user_info_database_save_path = 'data/database/user_info.json'
 
 
-mail_login_user = "2021023231@m.scnu.edu.cn"
-mail_password = "2RddzpookTYwJGwn"
+mail_login_user = ""
+mail_password = ""
 mail_sender = mail_login_user
 from_str = 'GPU服务器管理员'
 server_host = "smtp.exmail.qq.com"
@@ -15,4 +15,4 @@ server_port = 465
 use_ssl = True
 max_resend_time = 5
 
-administrator_passwd = "2021023231"
+administrator_passwd = ""
