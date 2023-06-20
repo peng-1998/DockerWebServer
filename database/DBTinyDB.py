0b344aea53f8a97c44e07504248cfe88f96ea21a
peng-1998 <55737967+peng-1998@users.noreply.github.com>
2023-06-20 06:21:18 +0000

    change base db

--- database/DBTinyDB.py
+++ database/DBTinyDB.py
@@ -1,6 +1,5 @@
 import os
 from tinydb import TinyDB, Query
-from typing import N
 from .BaseDB import BaseDB
 
 
@@ -14,9 +13,13 @@ class DB_TinyDB(BaseDB):
         self.image_table = self.db.table('Image')
         self.container_table = self.db.table('Container')
         self.machine_table = self.db.table('Machine')
+        self.user_table.purge()
         self.query = Query()
 
     def insert_user(self, user: dict) -> bool:
+        for key in self.user_key:
+            if key not in user:
+                if 
         user_id = self.user_table.insert(user)
         return bool(user_id)
 
@@ -29,11 +32,10 @@ class DB_TinyDB(BaseDB):
         return users
 
     def update_user(self, search_key: dict, update_key: dict) -> bool:
-
-        pass
+        return bool(self.user_table.update(update_key, self.query.fragment(search_key)))
 
     def delete_user(self, search_key: dict) -> bool:
-        pass
+        return bool(self.user_table.remove(self.query.fragment(search_key)))
 
     def insert_image(self, image: dict) -> bool:
         pass
