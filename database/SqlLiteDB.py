e2635e80c3eab79ccf0d41d214691b37ce1753c6
meihuanyuyuyu <1361577068@qq.com>
2023-06-25 06:55:36 +0000

    sqllite

--- database/SqlLiteDB.py
+++ database/SqlLiteDB.py
@@ -43,3 +43,92 @@ class SqlLiteDB(BaseDB):
             (v for v in search_key.values()),
         )
         self.db.commit()
+
+    def _get(
+        self,
+        table_name: str,
+        search_key: dict,
+        return_key: list = None,
+        limit: int = None,
+    ):
+        self.cursor.execute(
+            f"SELECT {', '.join(return_key)} FROM {table_name.lower()} WHERE {f' AND '.join([f'{k} = ?' for k,v in search_key.keys()])} LIMIT = ?",
+            (v for v in search_key.values()) + (limit,),
+        )
+        info = self.cursor.fetchall()
+        return info
+
+    def _update(self, table_name: str, search_key: dict, update_key: dict):
+        self.cursor.execute(
+            f"UPDATE {table_name.lower()} SET {', '.join([f'{k} = ?' for k in update_key.keys()])} WHERE {f' AND '.join([f'{k} = ?' for k in search_key.keys()])}",
+            (v for v in update_key.values()) + (v for v in search_key.values()),
+        )
+        self.db.commit()
+
+    def get_user(
+        self, search_key: dict, return_key: list = None, limit: int = None
+    ) -> list:
+        return self._get("user", search_key, return_key, limit)
+
+    def get_image(
+        self, search_key: dict, return_key: list = None, limit: int = None
+    ) -> list:
+        return self._get("image", search_key, return_key, limit)
+
+    def get_container(
+        self, search_key: dict, return_key: list = None, limit: int = None
+    ) -> list:
+        return self._get("container", search_key, return_key, limit)
+
+    def get_machine(
+        self, search_key: dict, return_key: list = None, limit: int = None
+    ) -> list:
+        return self._get("machine", search_key, return_key, limit)
+
+    def insert_user(self, user: dict) -> bool:
+        self._insert("user", user)
+        return True
+
+    def insert_image(self, image: dict) -> bool:
+        self._insert("image", image)
+        return True
+
+    def insert_container(self, container: dict) -> bool:
+        self._insert("container", container)
+        return True
+
+    def insert_machine(self, machine: dict) -> bool:
+        self._insert("machine", machine)
+        return True
+
+    def delete_user(self, search_key: dict) -> bool:
+        self._delete("user", search_key)
+        return True
+
+    def delete_image(self, search_key: dict) -> bool:
+        self._delete("image", search_key)
+        return True
+
+    def delete_container(self, search_key: dict) -> bool:
+        self._delete("container", search_key)
+        return True
+
+    def delete_machine(self, search_key: dict) -> bool:
+        self._delete("machine", search_key)
+        return True
+
+    def update_user(self, search_key: dict, update_key: dict) -> bool:
+        self._update("user", search_key, update_key)
+        return True
+
+    def update_image(self, search_key: dict, update_key: dict) -> bool:
+        self._update("image", search_key, update_key)
+        return True
+
+    def update_container(self, search_key: dict, update_key: dict) -> bool:
+        self._update("container", search_key, update_key)
+        return True
+
+    def update_machine(self, search_key: dict, update_key: dict) -> bool:
+        self._update("machine", search_key, update_key)
+        return True
