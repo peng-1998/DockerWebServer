7036fe9de5d07db3761f5b8b3e713c4f189da614
peng-1998 <55737967+peng-1998@users.noreply.github.com>
2023-06-30 08:39:43 +0000

    add admin page

--- communication/Admin.py
+++ communication/Admin.py
@@ -1,1 +1,26 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint , request
+
+
+admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin.route('/', methods=['GET'])
+def index():
+    ...
+
+
+@admin.route('/build_image', methods=['POST'])
+def build_image():
+    attrs = request.json
+    dockerfile = attrs['dockerfile']
+    image_name = attrs['image_name']
+    tag = attrs['tag']
+    showname = attrs['showname']
+    creat_args = attrs['creat_args']
+    description = attrs['description']
+    ...
+
+@admin.route('/delete_image', methods=['POST'])
+def delete_image():
+    attrs = request.json
+    image_id = attrs['image_id']
+    ...
